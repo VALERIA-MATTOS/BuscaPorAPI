bc8833c23cd278d133ddc0221d43d465622e639e
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-05-31 16:58:08 -0300

    arrumando a exibicao dos titulos

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,10 @@ var endereco= {
   itens:'https://oc-index.library.ubc.ca/collections/'
 }
 
-var cabecalho='<table class="table table-striped table-bordered"><tr><th>Código</th><th>Título da coleção</th></tr>';
+var cabecalho={
+  colecao:'<table class="table table-striped table-bordered"><tr><th>Código</th><th>Título da coleção</th></tr>',
+  item:'<br><table class="table table-striped table-bordered" id="tabelaInfos"><tr><th>Propriedades</th><th>Informações</th></tr>'
+}
 
 $(document).ready(function(){
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
@@ -49,13 +52,23 @@ $(document).ready(function(){
   $('#tabelaAcervo1').show();
 })
 
+function esconder(itens){
+  for(var x=0; x<itens.length; x++){
+    $(itens[x]).stop().hide();
+  }
+}
+
+function escondeTabelas(){
+  esconder(['#tabelaAcervo1','#tabelaAcervo2','#tabelaAcervo3','#tabelaAcervo4','#tabelaAcervo5','#tabelaAcervo6','#tabelaAcervo7','#tabelaAcervo8','#tabelaAcervo9','#tabelaAcervo10','#tabelaAcervo11','#tabelaAcervo12','#tabelaAcervo13','#tabelaAcervo14','#tabelaAcervo15']);
+}
+
 function limparResultadoPesquisa(){
   $('#tabelaInformacoes').html('');
 }
 
 function limparCampoPesquisa (){
   $('#campoPesquisa').val('');
-  $('#selecaoItem').hide();
+  esconder(['#selecaoItem']);
   $("#myModal").modal();
 }
 
@@ -63,6 +76,44 @@ function validacaoColecao (){
   var codigo=$('#campoPesquisa').val();
   if (codigo!=0) pesquisarColecao(codigo);
   else limparCampoPesquisa();
+}
+
+function acervo(){
+  $.getJSON(endereco.colecoes, function(data){
+    var result='';
+    var tituloColecao='';
+    var x=0;
+    var c=0;
+    result+=cabecalho.colecao;
+    for (var n=0; n<373; n++){
+      x++;
+      if (data.data[n]==undefined) {
+        do {
+          n++;
+        } while(data.data[n]==undefined);
+      }
+      result+='<tr><td>' + data.data[n] + '</td>';
+      var acervoCodigo = data.data[n];
+      tituloColecao=acervoTitulos(acervoCodigo);
+      result+='<td>'+ tituloColecao +' </td></tr>';
+      if (x===10){
+        x=0;
+        c++;
+        result += '</table>';
+        $('#tabelaAcervo'+ c).html(result);
+        result=cabecalho.colecao;
+      }
+    }
+  });
+}
+
+function acervoTitulos(acervoCodigo){
+  var titulo = '';
+  $.getJSON(endereco.colecoes + '/' + acervoCodigo, function(data){
+      titulo=(data.data.title);
+      return titulo;
+  });
+  console.log(titulo);
 }
 
 function pesquisarColecao(codigo){
@@ -82,57 +133,24 @@ function pesquisarColecao(codigo){
 function tabelaItens (){
   var codigo=$('#campoPesquisa').val();
   var itemSelecionado = $('#selecaoItem').val();
-  $.getJSON(endereco.itens + codigo + '/items/' + itemSelecionado, function(data){
-    var result ='';
-    result+='<br><table class="table table-striped table-bordered" id="tabelaInfos"><tr><th>Propriedades</th><th>Informações</th></tr>';
-    if ($(data.data.Creator).length != 0) result+='<tr><td> Criador </td><td>' + data.data.Creator[0].value + '</td></tr>';
-    if ($(data.data.Contributor).length != 0) result+='<tr><td> Contribuidores </td><td>' + data.data.Contributor[0].value + '</td></tr>';
-    if ($(data.data.Collection).length !=0) result+='<tr><td> Coleção </td><td>' + data.data.Collection[0].value + '</td></tr>';
-    if ($(data.data.DateCreated).length !=0) result+='<tr><td> Data de criação </td><td>' + data.data.DateCreated[0].value + '</td></tr>';
-    if ($(data.data.DateIssued).length !=0) result+='<tr><td> Data de emissão </td><td>' + data.data.DateIssued[0].value + '</td></tr>';
-    if ($(data.data.Edition).length !=0) result+='<tr><td> Edição </td><td>' + data.data.Edition[0].value + '</td></tr>';
-    if ($(data.data.Genre).length !=0) result+='<tr><td> Gênero </td><td>' + data.data.Genre[0].value + '</td></tr>';
-    if ($(data.data.Language).length !=0) result+='<tr><td> Idioma </td><td>' + data.data.Language[0].value + '</td></tr>';
-    if ($(data.data.Country).length !=0) result+='<tr><td> País </td><td>' + data.data.Country[0].value + '</td></tr>';
-    if ($(data.data.Title).length !=0) result+='<tr><td> Título </td><td>' + data.data.Title[0].value + '</tr></table>';
-    $('#tabelaInformacoes').html(result);
-  })
-}
-
-function acervo(){
-  $.getJSON(endereco.colecoes, function(data){
-    var result='';
-    var x=0;
-    var c=0;
-    result+=cabecalho;
-    for (var n=0; n<373; n++){
-      x++;
-      if (data.data[n]==undefined) {
-        do {
-          n++;
-        } while(data.data[n]==undefined);
-      }
-      result+='<tr><td>' + data.data[n] + '</td>';
-      result+='<td>'+ c +' </td></tr>';
-      if (x===15){
-        x=0;
-        c++;
-        result += '</table>';
-        $('#tabelaAcervo'+ c).html(result);
-        result=cabecalho;
-      }
-    }
-  });
-}
-
-function limpa(itens){
-  for(var x=0; x<itens.length; x++){
-    $(itens[x]).stop().hide();
+  if (itemSelecionado!="#") {
+    $.getJSON(endereco.itens + codigo + '/items/' + itemSelecionado, function(data){
+      var result ='';
+      result+=cabecalho.item;
+      if ($(data.data.Creator).length != 0) result+='<tr><td> Criador </td><td>' + data.data.Creator[0].value + '</td></tr>';
+      if ($(data.data.Contributor).length != 0) result+='<tr><td> Contribuidores </td><td>' + data.data.Contributor[0].value + '</td></tr>';
+      if ($(data.data.Collection).length !=0) result+='<tr><td> Coleção </td><td>' + data.data.Collection[0].value + '</td></tr>';
+      if ($(data.data.DateCreated).length !=0) result+='<tr><td> Data de criação </td><td>' + data.data.DateCreated[0].value + '</td></tr>';
+      if ($(data.data.DateIssued).length !=0) result+='<tr><td> Data de emissão </td><td>' + data.data.DateIssued[0].value + '</td></tr>';
+      if ($(data.data.Edition).length !=0) result+='<tr><td> Edição </td><td>' + data.data.Edition[0].value + '</td></tr>';
+      if ($(data.data.Genre).length !=0) result+='<tr><td> Gênero </td><td>' + data.data.Genre[0].value + '</td></tr>';
+      if ($(data.data.Language).length !=0) result+='<tr><td> Idioma </td><td>' + data.data.Language[0].value + '</td></tr>';
+      if ($(data.data.Country).length !=0) result+='<tr><td> País </td><td>' + data.data.Country[0].value + '</td></tr>';
+      if ($(data.data.Title).length !=0) result+='<tr><td> Título </td><td>' + data.data.Title[0].value + '</tr></table>';
+      $('#tabelaInformacoes').html(result);
+    })  
   }
-}
-
-function escondeTabelas(){
-  limpa(['#tabelaAcervo1','#tabelaAcervo2','#tabelaAcervo3','#tabelaAcervo4','#tabelaAcervo5','#tabelaAcervo6','#tabelaAcervo7','#tabelaAcervo8','#tabelaAcervo9','#tabelaAcervo10','#tabelaAcervo11']);
+  else limparResultadoPesquisa();
 }
 
 function paginacao (pagina){
@@ -180,6 +198,22 @@ function paginacao (pagina){
     case "11":
       escondeTabelas();
       $('#tabelaAcervo11').show();
+      break;
+    case "12":
+      escondeTabelas();
+      $('#tabelaAcervo12').show();
+      break;
+    case "13":
+      escondeTabelas();
+      $('#tabelaAcervo13').show();
+      break;
+    case "14":
+      escondeTabelas();
+      $('#tabelaAcervo14').show();
+      break;
+    case "15":
+      escondeTabelas();
+      $('#tabelaAcervo15').show();
       break;
   }
 }
