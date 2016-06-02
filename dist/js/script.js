31e5e6a057e95d6aadda57e5a81385eea828faa5
Valeria De Mattos Rex <valeria.rex@digitaldesk.com.br>
2016-06-02 10:00:11 -0300

    Funcao para animacao rolagem

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,27 +9,7 @@ var cabecalho={
 }
 
 $(document).ready(function(){
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-      var winTop = $(window).scrollTop();
-      if (pos < winTop + 600) {
-        $(this).addClass("slide");
-      }
-    });
-  });
+  efeitoRolagemTela ();
 
   $('#limpar').click(function(){
     limparResultadoPesquisa();
@@ -65,6 +45,30 @@ $(document).keypress(function(e) {
     validacaoColecao();
   }
 });
+
+function efeitoRolagemTela (){
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+}
 
 function limparResultadoPesquisa(){
   $('#tabelaInformacoes').html('');
